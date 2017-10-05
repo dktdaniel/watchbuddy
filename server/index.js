21c6d22d8a074704861b89240581e17bc1318ed9
Helen Yi <helenhyi@gmail.com>
2017-10-04 19:07:30 -0700

    resolve merge conflict

--- server/index.js
+++ server/index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database-mysql');
 const moviedb = require('../helper/moviedb.js');
-<<<<<<< HEAD
 
-=======
->>>>>>> 2cfa4cc48e7456dadc30a895d638b2107abd375a
 // var items = require('../database-mongo');
 
 const app = express();
 
 app.use(bodyParser.json());
-<<<<<<< HEAD
 
 
 app.use(express.static(__dirname + '/../react-client/dist'));
@@ -19,14 +15,12 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 app.get('/items', function (req, res) {
 	tvMaze.getInfoByTitle('Game of thrones', (data) => {
 		console.log(JSON.parse(data))
-=======
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 
 app.get('/recommend', function (req, res) {
 	moviedb.getPopularShows((data) => {
 		res.send(JSON.parse(data));
->>>>>>> 2cfa4cc48e7456dadc30a895d638b2107abd375a
 	})
 	
 });
@@ -35,7 +29,6 @@ app.post('/submit', function (req, res) {
 	var title = req.body.topic.split(' ').join('_')
 	moviedb.getInfoByTitle(title, (data) => {
 		res.send(JSON.parse(data))
-<<<<<<< HEAD
 	})
 })
 
@@ -51,7 +44,6 @@ app.post('/signUp', function (req, res) {
 	})
 })
 
-=======
 	})
 })
 
@@ -67,7 +59,6 @@ app.post('/signUp', function (req, res) {
 	})
 })
 
->>>>>>> 2cfa4cc48e7456dadc30a895d638b2107abd375a
 app.post('/logIn', function (req, res){
 	var user = req.body.username;
 	var pw = req.body.password;
